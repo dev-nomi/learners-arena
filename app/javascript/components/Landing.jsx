015a89b577de0655a2a97ce8a8f556bca43599a7
dev-nomi <noman.khalid.freelance@gmail.com>
2022-10-24 00:53:55 +0500

    Added modal for course and change the font family for html tags.

--- app/javascript/components/Landing.jsx
+++ app/javascript/components/Landing.jsx
@@ -8,6 +8,9 @@ import {
   Typography,
   Button,
   Container,
+  Box,
+  Modal,
+  Stack,
 } from "@mui/material";
 import axios from "axios";
 import { toast } from "react-toastify";
@@ -15,6 +18,22 @@ import { useSelector } from "react-redux";
 import { useNavigate } from "react-router-dom";
 import Errors from "../components/Errors";
 import { useTheme } from "@mui/material/styles";
+import SignalCellularAltIcon from "@mui/icons-material/SignalCellularAlt";
+import QueryBuilderIcon from "@mui/icons-material/QueryBuilder";
+import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
+
+const style = {
+  position: "absolute",
+  top: "50%",
+  left: "50%",
+  transform: "translate(-50%, -50%)",
+  width: 500,
+  bgcolor: "background.paper",
+  border: "2px solid #1A374D",
+  boxShadow: 24,
+  borderRadius: 1,
+  p: 4,
+};
 
 const Landing = () => {
   const [courses, setCourses] = useState([]);
@@ -22,6 +41,11 @@ const Landing = () => {
   const isLoggedIn = useSelector((state) => state.auth.isLoggedIn);
   const navigate = useNavigate();
   const theme = useTheme();
+  const [open, setOpen] = useState(false);
+  const [modalData, setModalData] = useState(null);
+
+  const handleOpen = () => setOpen(true);
+  const handleClose = () => setOpen(false);
 
   useEffect(() => {
     initialize();
@@ -60,7 +84,13 @@ const Landing = () => {
       <Grid container spacing={2} mt={2} mb={4}>
         {courses.map((course) => (
           <Grid item xs={4} key={course.id}>
-            <Card sx={{ maxWidth: 345 }}>
+            <Card
+              sx={{ maxWidth: 345 }}
+              onClick={() => {
+                handleOpen();
+                setModalData(course);
+              }}
+            >
               <CardMedia
                 component="img"
                 height="140"
@@ -93,6 +123,59 @@ const Landing = () => {
           </Grid>
         ))}
       </Grid>
+      <Modal
+        open={open}
+        onClose={handleClose}
+        aria-labelledby="modal-modal-title"
+        aria-describedby="modal-modal-description"
+      >
+        <Box sx={style}>
+          <Typography sx={{ color: theme.palette.primary.main }} variant="h6" component="h2">
+            {modalData?.display_name}
+          </Typography>
+          <Card
+            sx={{
+              marginTop: 2,
+              width: "100%",
+            }}
+          >
+            <CardContent>
+              <Typography variant="body2" color="text.secondary" sx={{ mb: 2 }}>
+                {modalData?.description}
+              </Typography>
+              <Stack direction="row" alignItems="center" gap={1}>
+                <SignalCellularAltIcon />
+                <Typography variant="body2" color="text.secondary">
+                  {modalData?.level}
+                </Typography>
+              </Stack>
+              <Stack direction="row" alignItems="center" gap={1}>
+                <QueryBuilderIcon />
+                <Typography variant="body2" color="text.secondary">
+                  {modalData?.total_hours} hours
+                </Typography>
+              </Stack>
+            </CardContent>
+          </Card>
+          <Card
+            sx={{
+              marginTop: 2,
+              width: "100%",
+            }}
+          >
+            <Typography
+              sx={{ color: theme.palette.primary.main, marginTop: 2, marginLeft: 2 }}
+              variant="h6"
+              component="h2"
+            >
+              Course Outline
+            </Typography>
+            <CardContent>
+              <div dangerouslySetInnerHTML={{ __html: modalData?.outline }} />
+            </CardContent>
+          </Card>
+        </Box>
+      </Modal>
     </Container>
   );
 };
